50036b912763e7c2218d9e35cf0aac099e32afdf
RenNguyen2001 <126844711+RenNguyen2001@users.noreply.github.com>
2023-05-12 22:29:24 +1000

    Add files via upload
    - Made USART work in synch mode. Results were too inaccurate in asynch mode at higher baud rates.

--- usart9600.h
+++ usart9600.h
@@ -10,6 +10,7 @@ void txByteUSART(unsigned char x){
 	UDR0 = x;
 }
 
+
 void txStringUSART(char *x){
 	while (*x != '\0')
 	{
@@ -18,12 +19,27 @@ void txStringUSART(char *x){
 	}
 }
 
+
 void txIntUSART(int x){
 	char array[10] = {};
 	dtostrf(x, 10, 0, array);
 	array[10] = '\0';
 	
 	txStringUSART(array);
+}
+
+void txLongUSART(long x){
+	char array[4] = {};
+	
+	//convert the long into bytes - a long consist of 4 bytes
+	for(int i = 0; i < 4; i++)
+	{
+		array[i] |= x >> i*8;
+		//wait for empty transmit buffer
+		while(!(UCSR0A & (1 << UDRE0)));
+		//Put data into buffer, sends the data
+		UDR0 = array[i];
+	}
 }
 
 void txFloatUSART(float x){
@@ -53,9 +69,19 @@ void initUSART9600()
 	UCSR0C = 0x06;	//3. set default frame format
 }
 
-void initUSART(){
-	UBRR0 = 103;
+
+void initUSART(long baudRate){	//lsb goes first
+	//NOTE - for baud rates higher than 85000, will need to create a 4MHZ signal for synchronous control
 	
-	UCSR0C |= 3 << UCSZ00;
-	UCSR0B |= 3 << TXEN0;
+	int regValue = (16e6/(16*baudRate));
+	
+	UBRR0L = regValue & 0xFF;
+	UBRR0H = regValue >> 8;
+	
+	UCSR0C |= (3 << UCSZ00);	//setting UCSZ00 and UCSZN1 for 8 bit character size
+	UCSR0C |= (1 << UMSEL00);	//setting to synchronous mode with UMSEL00 bit
+	//DDRD |= 1<<PIND4;
+	
+	UCSR0B |= 3 << TXEN0;	//enabling receiver and transmitter
+	//UCSR0A |= 1 << U2X0;		// double speed
 }
