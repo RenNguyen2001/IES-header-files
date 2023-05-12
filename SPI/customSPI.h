369c4895b3ba88cfef97967caf15f70621619fe7
RenNguyen2001 <126844711+RenNguyen2001@users.noreply.github.com>
2023-05-12 12:44:06 +1000

    Add files via upload

---
+++ SPI/customSPI.h
@@ -0,0 +1,116 @@
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "util/delay.h"
+#include <stdlib.h>
+
+//SPI PIN DEFINITIONS
+#define DDR_SPI DDRB
+#define pinSCK PB5	//pin 13
+#define pinMOSI PB3	//pin 11
+#define pinMISO PB4	//pin 12
+#define pinSS PB2	//pin 10
+
+const char oscFreq[] = {
+	0,		//	freq/4
+	1,		//	freq/16
+	2,		//	freq/64
+	3		//	freq/128
+};
+
+
+void transmitMasterSPI(char numBytes, long *pDataSpi)	//4 is the max bytes this function is intended for
+{
+	
+	//long *pDataSpi = 0xFFABCCDD;
+	PORTB &= ~(1 << pinSS);	//setting SLAVE SELECT low to initiate transfer
+	
+	
+	for(int i = numBytes - 1; i >= 0; i--)
+	{
+		SPDR = *pDataSpi >> i*8;
+		while(!(SPSR & (1 << SPIF)));	//checking if SPI interrupt flag isn't set/serial transfer not completed
+	}
+
+	PORTB |= (1 << pinSS); 	//setting SLAVE SELECT high to end transfer
+	
+}
+
+void transmitSlaveSPI(char numBytes, long *pDataSpi)
+{
+	for(int i = numBytes - 1; i >= 0; i--)
+	{
+		SPDR = *pDataSpi >> i*8;
+		while(!(SPSR & (1 << SPIF)));	//checking if SPI interrupt flag isn't set/serial transfer not completed
+	}
+}
+
+void tx_and_rx(char numBytes, unsigned long *pDataSpi)	//if the master wants to transmit and then receive
+{
+	PORTB &= ~(1 << pinSS);	//setting SLAVE SELECT low to initiate transfer
+	
+	
+	for(int i = numBytes - 1; i >= 0; i--)
+	{
+		SPDR = *pDataSpi >> i*8;
+		while(!(SPSR & (1 << SPIF)));	//checking if SPI interrupt flag isn't set/serial transfer not completed
+	}
+	
+	//_delay_ms(10);
+	
+	PORTB |= (1 << pinSS); 	//setting SLAVE SELECT high to end transfer
+	//_delay_ms(1);
+	
+}
+
+void setupMasterSPI(unsigned char oscSetting)
+{
+	//cli();
+	//set SS pin as output
+	DDR_SPI |= (1 << pinSS) | (1 << pinSCK) | (1 << pinMOSI);
+	
+	//enable SPI, set as master
+	SPCR |= (1 << SPE) | (1 << MSTR);
+	SPCR |= (oscFreq[oscSetting]); 
+	
+	//enable SPI receiving from slave
+	DDR_SPI &= ~ (1 << pinMISO);
+	
+	//enable the SPI interrupt flag to be reset after each byte for multi byte transfer
+	//SPCR |= 1 << SPIE;
+	//sei();
+}
+
+void setupSlaveSPI()
+{
+	cli();
+	
+	// enable SPI, set as slave
+	SPCR |= (1 << SPE) | (1 << SPIE);
+	sei();	//enable global interrupts
+	
+	// enable SPI transmitting slave->master
+	DDR_SPI |= 1 << pinMISO;
+	//bitSet(DDR_SPI, pinMISO);
+	sei();
+}
+
+void receiveSPI(char numBytes, long *pRxData)	//returns pointer to data
+{
+	if(!( DDR_SPI & (1 << pinSS) ) ){	//slave waits for SS to be low (waiting for master to transfer)
+		//store each incoming byte
+		*pRxData = SPDR;
+		while (!(SPSR & (1 << SPIF)));	//checking for SPI interrupt flag in SPI status register/completion of serial transfer
+		
+		*pRxData = SPDR;
+		while (!(SPSR & (1 << SPIF)));	//checking for SPI interrupt flag in SPI status register/completion of serial transfer
+		
+		*pRxData = SPDR;
+		while (!(SPSR & (1 << SPIF)));	//checking for SPI interrupt flag in SPI status register/completion of serial transfer
+		
+		*pRxData = SPDR;
+		while (!(SPSR & (1 << SPIF)));	//checking for SPI interrupt flag in SPI status register/completion of serial transfer
+		
+	}
+}
+
